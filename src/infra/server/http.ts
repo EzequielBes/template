6229f30be8bbc60440f0eb93afe6059cad4f24a7
ezequielbes <ezequieltdbeserra@gmail.com>
2025-01-21 08:26:56 -0300

    first commit

---
+++ src/infra/server/http.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import { HttpServer } from "../../interfaces/IServer"
+
+export class ExpressHttpServer implements HttpServer {
+    app
+    constructor () {
+        this.app = express()
+        this.app.use(express.json())
+    }
+
+    
+    register(method: string, url: string, callBack: Function): void {
+        throw new Error("Method not implemented.")
+    }
+    listen(port: number): void {
+        throw new Error("Method not implemented.")
+    }
+
+
+}
---
+++ src/interfaces/IServer.ts
@@ -0,0 +1,5 @@
+export interface HttpServer {
+    register (method:string, url:string, callBack:Function):void
+    listen (port: number):void
+}
+
